dcf730cafa3e4f5555ae2f0442d5fbbd28d7bc59
aviv <aviv@innovid.com>
2019-04-04 00:14:43 +0300

    init

---
+++ submissions/last-place/build/precache-manifest.13069c966b026044c2b3516efa3c6aaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "069af89cbc4556048df0",
+    "url": "/static/js/main.93d397d4.chunk.js"
+  },
+  {
+    "revision": "00f756abc775cb4e0ace",
+    "url": "/static/js/2.28dfb6bc.chunk.js"
+  },
+  {
+    "revision": "069af89cbc4556048df0",
+    "url": "/static/css/main.0d260751.chunk.css"
+  },
+  {
+    "revision": "104363f5e291b86a62afd739f65f360d",
+    "url": "/index.html"
+  }
+];
